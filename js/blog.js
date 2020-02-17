84412d1ab44824eec8fdf1a65694e866f6c97fb9
FranPaya19911 <paya19911@iesmarenostrum.com>
2020-02-17 20:12:51 +0100

    z

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,50 @@
 $(function() {
-
+    $("aside.Entrada1").click(function() {
+        var posicion = $("article.Entrada1").offset();
+        $("html").animate({
+            scrollTop: position.top
+        }, "slow")
+    })
+    $("aside.Entrada2").click(function() {
+        var posicion2 = $("article.Entrada2").offset();
+        $("html").animate({
+            scrollTop: position2.top
+        }, "slow")
+    })
+    $("aside.Entrada3").click(function() {
+        var posicion = $("article.Entrada3").offset();
+        $("html").animate({
+            scrollTop: position.top
+        }, "slow")
+    })
+    $("aside.Entrada4").click(function() {
+        var posicion = $("article.Entrada4").offset();
+        $("html").animate({
+            scrollTop: position.top
+        }, "slow")
+    })
+    $("#Entrada5").click(function() {
+        var posicion5 = $("#article_Entrada5").offset();
+        $("html").animate({
+            scrollTop: posicion5.top
+        }, "slow")
+    })
+    $("aside.#Entrada6").click(function() {
+        var posicion = $("article.#Entrada6").offset();
+        $("html").animate({
+            scrollTop: position.top
+        }, "slow")
+    })
+    $("aside.Entrada7").click(function() {
+        var posicion = $("article.Entrada7").offset();
+        $("html").animate({
+            scrollTop: position.top
+        }, "slow")
+    })
+    $("aside.Entrada8").click(function() {
+        var posicion = $("article.Entrada8").offset();
+        $("html").animate({
+            scrollTop: position.top
+        }, "slow")
+    })
 })
