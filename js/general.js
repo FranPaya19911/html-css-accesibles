44b0b6eba91a102e97ba952d8742e5d6d9bb839b
FranPaya19911 <paya19911@iesmarenostrum.com>
2020-02-18 20:49:49 +0100

    ss

--- js/general.js
+++ js/general.js
@@ -1,3 +1,10 @@
 $(function() {
-
+    $(".flecha").css("display", "block");
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 500) {
+            $("#flecha").fadeIn("slow");
+        } else {
+            $("#flecha").fadeOut("slow");
+        }
+    });
 })
