54c6330243750d49fead50571ac67dced884d9bf
FranPaya19911 <paya19911@iesmarenostrum.com>
2020-02-19 18:56:26 +0100

    viewport, bars , close

--- js/general.js
+++ js/general.js
@@ -1,10 +1,18 @@
 $(function() {
-    $(".flecha").css("display", "block");
+    $(".flecha").css("display", "none");
     $(window).scroll(function() {
         if ($(window).scrollTop() > 500) {
-            $("#flecha").fadeIn("slow");
+            $(".flecha").fadeIn("slow");
         } else {
-            $("#flecha").fadeOut("slow");
+            $(".flecha").fadeOut("slow");
         }
     });
+    $(".flecha").click(function() {
+        var posicion = $("h1").offset();
+        $("html").animate({
+            scrollTop: posicion.top - 100
+        }, "slow")
+    })
+
+
 })
